ebaa99c465a35e8cf7209c13a1dc6bfab5cba35f
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-07 21:36:16 +0200

    improve hero section responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,11 +11,11 @@ interface HeroProps {
 const Hero: React.FC<HeroProps> = ({ title, subtitle, buttonText, imageUrl, onButtonClick }) => {
   return (
     <section className="flex flex-col lg:flex-row items-center justify-center h-screen  text-grey px-6">
-      <div className="lg:w-1/2 max-w-xl lg:text-left text-center mb-8 lg:mb-0">
+      <div className="lg:w-1/2 max-w-xl lg:text-left  text-center mb-8 lg:mb-0">
         <h1 className="text-4xl lg:text-5xl font-bold mb-4">{title}</h1>
         <p className="text-lg mb-6">{subtitle}</p>
         <a href="#shorten-link-input">
-          <button onClick={onButtonClick} className="btnPrimary rounded-full block px-4 py-2 text-white">
+          <button onClick={onButtonClick} className="btnPrimary lg:block rounded-full px-4 py-2 text-white">
           {buttonText}
           </button>
         </a>
